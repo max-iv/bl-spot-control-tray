246567e74ae228de0cfd0b34870dc6ee68680098
David McGinnis <david.mcginnis@bl-mc.se>
2024-05-05 06:29:29 +0200

    added pm2 name

--- pm2.sh
+++ pm2.sh
@@ -4,6 +4,6 @@ cd $*
 while read line; do
     export $line
 done < .env
-pm2 start node_modules/node-red/red.js --node-args="--max-old-space-size=384" --restart-delay 10000 -- -s ./settings.js -u ./
+pm2 start node_modules/node-red/red.js --name $PM2NAME --node-args="--max-old-space-size=384" --restart-delay 10000 -- -s ./settings.js -u ./
 
 
